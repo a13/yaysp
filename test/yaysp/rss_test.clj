75b491a26a49ccb6072c9a8b1d8d4115e97d1d6c
DK <beerandhot@gmail.com>
2017-05-31 20:54:32 +0300

    fix fn name

--- test/yaysp/rss_test.clj
+++ test/yaysp/rss_test.clj
@@ -10,6 +10,6 @@
    (= (rss/parse rss-str)
       '("https://www.linux.org.ru/forum/development/13451999" "" "https://groups.google.com/d/topic/ror2ru/2hdkOK1yvWU" "https://groups.google.com/d/topic/ror2ru/2hdkOK1yvWU" "http://www.sql.ru/forum/1261520/tyapnichnyy-benchmark-cpu-part-zero" "http://www.jdon.com/48916" "https://jobs.dou.ua/companies/attendify/vacancies/42879/?1494936667" "http://rubyjobs.ru/vacancies/4805" "http://lisper.ru/forum/messages/12276" "http://lisper.ru/forum/messages/12266")))
   (is (thrown? javax.xml.stream.XMLStreamException
-               (rss/process "")))
+               (rss/parse "")))
   (is (thrown? javax.xml.stream.XMLStreamException
-               (rss/process "<malformed shit?"))))
+               (rss/parse "<malformed shit?"))))
